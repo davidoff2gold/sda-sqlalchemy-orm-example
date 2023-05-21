05c7f674948223b2cdc9273c9c4a945f5885d713
davidoff2gold <davidoff2gold@gmail.com>
2023-05-21 15:43:44 +0200

    Initial comment.

--- session.py
+++ session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    "mysql+pymysql://root:bk6yq5chf@localhost:3306/blog"
+    "mysql+pymysql://root:qwerty@localhost:3306/blog"
 )
 Session = sessionmaker(bind=engine)  # klasa
 session = Session()  # instancja klasy
