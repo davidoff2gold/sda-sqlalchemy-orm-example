b779fccea8e900b6a6f1cbda16cf0f716a067519
davidoff2gold <davidoff2gold@gmail.com>
2023-05-21 15:21:57 +0200

    Initial comment.

--- bootstrapdb.py
+++ bootstrapdb.py
@@ -1,18 +1,41 @@
+from faker import Faker
+from sqlalchemy.exc import IntegrityError
+
 from models import Base, User
 from session import session
 
+
+def create_users(count=50):
+    fake = Faker()
+    return [
+        User(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            user_name=fake.user_name(),
+            salary=fake.pyfloat(
+                min_value=4000,
+                max_value=10_000
+            ),
+            email=fake.email(),
+        )
+        for _ in range(count)
+    ]
+
+
 def main():
+    # Create all tables
     Base.metadata.create_all()
 
-    user = User(
-        first_name="John",
-        last_name="Doe",
-        user_name="johndoe",
-        email="john.doe@gmail.com",
-    )
-
-    session.add(user)
-    session.commit()
+    # Create users
+    users = create_users(count=1001)
+    for user in users:
+        print(f"Adding user {user.user_name}")
+        try:
+            session.add(user)
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+            print(f"User {user.user_name} already exists")
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,11 +1,9 @@
 import datetime
-from sqlalchemy import Integer, Column, String, DateTime
+from sqlalchemy import Integer, Column, create_engine, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
-
 from session import engine
 
 Base = declarative_base(bind=engine)
-
 
 class User(Base):
     __tablename__ = "users"
@@ -13,9 +11,10 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(50))
     last_name = Column(String(50))
-    user_name = Column(String(50), unique=True, nullable=False)
+    user_name = Column(String(50))
+    salary = Column(Float, default=0)
     email = Column(String(50), unique=True, nullable=False)
     registration_date = Column(DateTime, default=datetime.datetime.now)
 
     def __repr__(self):
-        return f"User({self.user_name}"
+        return f'User({self.user_name})'
